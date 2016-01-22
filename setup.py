7f56fbacbea7b5a1763737beba59668a7cdcec57
FuGangqiang <fu_gangqiang@163.com>
2016-01-22 15:51:56 +0800

    init repo

---
+++ setup.py
@@ -0,0 +1,29 @@
+import os
+from setuptools import setup
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.rst')) as f:
+    long_description = f.read()
+
+
+setup(
+    name='langconverter',
+    version='0.0.0',
+    description='Language codes converter',
+    long_description=long_description,
+    url='https://github.com/FuGangqiang/',
+    author='FuGangqiang',
+    author_email='fu_gangqiang@qq.com',
+    keywords='nltk langid bing',
+    license='MIT',
+
+    py_modules=['langconverter'],
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
